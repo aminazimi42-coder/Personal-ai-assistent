636a124bdf955f44a86463dbcaaf61221f01c755
Hermes Workspace <hermesdev@Amins-MacBook-Air.local>
2026-08-31 06:16:56 +0200

    chore(revert): remove artifacts created by agent in session

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: uvicorn aegis_main:app --host 0.0.0.0 --port $PORT
--- aegis_agent/simulation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Simulation sandbox for predictive execution checks."""
-
-from .sandbox import PredictiveSimulationSandbox, SimulationResult
-
-__all__ = ["PredictiveSimulationSandbox", "SimulationResult"]
